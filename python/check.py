7b29e5e895fabf0192e5fe786f21a6dfda778715
Y <y@MacBook-Air-Y-2.local>
2023-08-24 16:37:25 +0200

    upgreade 20230824

--- python/check.py
+++ python/check.py
@@ -1,4 +1,25 @@
-a = '563'
-b = '573'
+from random import randint
 
-print(a > b)
+a = []
+for i in range(10):
+    a.append(randint(1, 50))
+
+print(a)
+
+#a = [int(x) for x in a]
+
+def sortion_function(a):
+#	if i is None:
+#		i = -1
+
+#	i += 1 
+	pl = len(a)
+	for i in range(len(a)):
+		sortion_function(a[pl - 1])
+		print(sortion_function(a[pl - 1]))
+#			if a[i] > a[i+1] and i+1 < (pl -1):
+#				a[i], a[i+1] = a[i+1], a[i]
+	return a
+
+sortion_function(a)
+print(a)
