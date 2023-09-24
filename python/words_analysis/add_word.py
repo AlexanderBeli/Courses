abf0c6507251ad746b49c0dad025cdb82734e316
Y <y@MacBook-Air-Y-2.local>
2023-09-24 21:37:28 +0200

    Update 3.1 show del_category

--- python/words_analysis/add_word.py
+++ python/words_analysis/add_word.py
@@ -25,15 +25,24 @@ if __name__ == "__main__":
 	dict_source = 'eng_dict.pickle'
 	type_source = 'abbreviation.txt'
 
-with open(dict_source, 'rb') as f:
-	dict_data = pickle.load(f)
-
 posetive_answers = ('yes', 'y')
 negative_answers = ('no', 'n')
+first_message = "What do you want to do? Type the one of the next commands: show, add, change, delete or exit. "
+first_answer_show = ('show', 's', 'sh', 'sho', 'show')
+first_answer_add = ('add', 'ad', 'a')
+first_answer_change = ('change', 'chang', 'chan', 'ch', 'c')
+first_answer_delete = ('delete', 'delet', 'dele', 'del', 'de', 'd')
+first_answer_exit = ('exit', 'exi', 'ex', 'e')
 middle_message = "We're checking that "
 final_message = "There is no another option here. You may restart the program or connect to the developers. Goodbye! "
+error_message = "There is no such relevant information in the dictionary. Please check what your wrote and try again. "
 result_message = "We've added it to the dictionary. "
-choice_message = "Please choose and write one of the next categories: word, meaning, collocations, categories"
+choice_message = "Please choose and write one of the next categories: word, meaning, collocations, categories "
+choice_answer_word = ('word', 'wor', 'wo', 'w')
+choice_answer_meaning = ('meaning', 'meanin', 'meani', 'mean', 'mea', 'me','m')
+choice_answer_collocations = ('collocations', 'collocation', 'collocatio','collocati', 'collocat', 'colloca', 'colloc', 'collo', 'coll', 'col', 'co', 'c')
+choice_answer_category = ('category', 'categor', 'catego', 'categ', 'cate', 'cat', 'ca')
+#при запросе ввода информации указать: Если вы хотите выйти из режима/завершить напишите 'exit'
 
 class Check:
 	def __init__(self, stroke):
@@ -82,7 +91,7 @@ class Check:
 		if w_type in type_data:
 			return 1
 		else:
-			print("The category ", w_type, "doesn't exist. Please try again or check the categories. ")
+			print("The category ", w_type, " doesn't exist. Please try again or check the categories. ")
 
 	def check_collocations_in_category(self, name, category, data):
 		self.name = name
@@ -105,11 +114,55 @@ class Check:
 	#Object of type set is not JSON serializable
 	#Стал использовать pickle 21.09.2023
 
-class Query():
+
+class ShowVoc:
 	def __init__(self, stroke):
 		self.stroke = stroke
 
-	def key_word_step(self, ans):
+		if not isinstance(stroke, str):
+			print("It's not a string. Please check what you typed.")
+			return None
+
+	def show_words(self, ans):
+		self.ans = ans
+		print(sorted(list(dict_data.keys())))
+
+	def show_meaning(self, ans):
+		self.ans = ans
+		name = input("Write the WORD that meaning you want to read: ")
+
+		if name in dict_data.keys():
+			for i, k in dict_data[name]['meaning'].items():
+				print(f"{name} \n {i}:\n {k}\n")
+		else:
+			print(error_message)
+
+	def show_collocations(self, ans):
+		self.ans = ans
+		name = input("Write the WORD that collocations you want to read: ")
+
+		if name in dict_data.keys():
+			for i, k in dict_data[name]['collocations'].items():
+				collocations_data = sorted(list(dict_data[name]['collocations'][i]))
+				print(f"{name} \n {i}:\n {collocations_data}\n")
+		else:
+			print(error_message)
+
+	def show_category(self, ans):
+		self.ans = ans
+
+		print(category_list_data)
+
+
+class AddVoc:
+	def __init__(self, stroke):
+		self.stroke = stroke
+
+		if not isinstance(stroke, str):
+			print("It's not a string. Please check what you typed.")
+			return None
+
+	def add_key_word_step(self, ans):
 		self.ans = ans
 
 		name = input('Please type here the WORD that you want to add to the dictionary: ')
@@ -187,114 +240,152 @@ class Query():
 		else:
 			print(name, " is not in the dictionary. Please try again or recheck the word. ")
 
-#след усовуршенствованная логика процесса Update 3.0
+	def add_category_step(self, ans):
+		self.ans = ans
+
+		name = input('Please type here the CATEGORY ABBREVIATION that you want to add the list: ')		
+		print(middle_message)
+
+		#осуществим проверку
+		if name not in category_list_data:
+			check_in = Check(name)
+			if check_in.check_w(name) == 1:
+				with open(type_source, 'a', encoding='utf-8') as add_category_list:
+					add_category_list.write(f"{name} \n")
+				print("The category ", name, " has added. ")
+		else:
+			print("The category ", name, "already exists. ")
+
+
+class ChangeVoc:
+	def __init__(self, stroke):
+		self.stroke = stroke
+
+		if not isinstance(stroke, str):
+			print("It's not a string. Please check what you typed.")
+			return None
+
+
+class DeleteVoc:
+	def __init__(self, stroke):
+		self.stroke = stroke
+
+		if not isinstance(stroke, str):
+			print("It's not a string. Please check what you typed.")
+			return None
+
+	def del_category(self, ans):
+		self.ans = ans
+
+		del_info = input("Write the category that you want to delete from the list: ")
+		list_category = category_list_data.split(f'\n')
+		if del_info in list_category:
+			new_category_list_data = category_list_data.replace(f'{del_info}\n', '')
+			print(new_category_list_data)
+
+			with open(type_source, 'w', encoding='utf-8') as new_category_list:
+				new_category_list.write(new_category_list_data)
+				print("The category ", del_info, " has removed. ")
+		else:
+			print(error_message)
+
+#усовершенствованная логика процесса Update 3.0
 
 while True:
-	step = input("What do you want to do? Type the one of the next commands: show, add, change, delete or exit. ")
-	process = Query(step)
+	with open(dict_source, 'rb') as f:
+		dict_data = pickle.load(f)
 
-	if step == 'show':
+	with open(type_source, 'r', encoding='utf-8') as category_list:
+		category_list_data = category_list.read()
+
+	step = input(first_message)
+
+	if step in first_answer_show:
 		step2 = input(choice_message)
-			if step2 == 'word':
-				print(middle_message)
+		process = ShowVoc(step)
 
-			elif step2 == 'meaning':
-				print(middle_message)
+		if step2 in choice_answer_word:
+			print(middle_message)
+			process.show_words(step2)
 
-			elif step2 == 'collocations':
-				print(middle_message)
+		elif step2 in choice_answer_meaning:
+			print(middle_message)
+			process.show_meaning(step2)
 
-			elif step2 == 'categories':
-				print(middle_message)
+		elif step2 in choice_answer_collocations:
+			print(middle_message)
+			process.show_collocations(step2)
 
-			else:
- 				print(final_message)
-
-	elif step == 'add':
-		step2 = input(choice_message)
-			if step2 == 'word':
-				print(middle_message)
-
-			elif step2 == 'meaning':
-				print(middle_message)
-
-			elif step2 == 'collocations':
-				print(middle_message)
-				process.add_collocations_step(step)
-				
-			elif step2 == 'categories':
-				print(middle_message)
-
-			else:
- 				print(final_message)
-
-	elif step == 'change':
-		step2 = input(choice_message)
-			if step2 == 'word':
-				print(middle_message)
-
-			elif step2 == 'meaning':
-				print(middle_message)
-
-			elif step2 == 'collocations':
-				print(middle_message)
-
-			elif step2 == 'categories':
-				print(middle_message)
-
-			else:
- 				print(final_message)
-
-	elif step == 'delete':
-		step2 = input(choice_message)
-			if step2 == 'word':
-				print(middle_message)
-
-			elif step2 == 'meaning':
-				print(middle_message)
-
-			elif step2 == 'collocations':
-				print(middle_message)
-
-			elif step2 == 'categories':
-				print(middle_message)
-
-			else:
- 				print(final_message)
-
-	elif step == 'exit':
-		break
-
-	else:
-		print(final_message)
-		break
-'''
-#Логика процесса Update 2.0
-step = input('Do you want to add the new word to the English dictionary? Use yes/no: ')
-process_one = Query(step)
-
-if step in posetive_answers:	
-	process_one.key_word_step(step)
-
-elif step in negative_answers:
-	step2 = input('Do you want to add some extra information or collocations to the existant word in the English dictionary? Use yes/no: ')
-
-	if step2 in posetive_answers:
-		step3 = input('Do you want to add the meaning of the existant word in the English dictionary? Use yes/no: ')
-
-		if step3 in posetive_answers:
-			process_one.add_meaning_step(step3)
+		elif step2 in choice_answer_category:
+			print(middle_message)
+			process.show_category(step2)
 
 		else:
-			step4 = input('Do you want to add collocations to the existant word in the English dictionary? Use yes/no: ')
+ 			print(final_message)
 
-			if step4 in posetive_answers:
-				process_one.add_collocations_step(step4)
+	elif step in first_answer_add:
+		step2 = input(choice_message)
+		process = AddVoc(step)
 
-			else:
-				print(final_message)
+		if step2 in choice_answer_word:
+			print(middle_message)
+			process.add_key_word_step(step2)
+
+		elif step2 in choice_answer_meaning:
+			print(middle_message)
+			process.add_meaning_step(step2)
+
+		elif step2 in choice_answer_collocations:
+			print(middle_message)
+			process.add_collocations_step(step2)
+
+		elif step2 in choice_answer_category:
+			print(middle_message)
+			process.add_category_step(step2)
+
+		else:
+ 			print(final_message)
+
+	elif step in first_answer_change:
+		step2 = input(choice_message)
+		if step2 in choice_answer_word:
+			print(middle_message)
+
+		elif step2 in choice_answer_meaning:
+			print(middle_message)
+
+		elif step2 in choice_answer_collocations:
+			print(middle_message)
+
+		elif step2 in choice_answer_category:
+			print(middle_message)
+
+		else:
+ 			print(final_message)
+
+	elif step in first_answer_delete:
+		step2 = input(choice_message)
+		process = DeleteVoc(step)
+
+		if step2 in choice_answer_word:
+			print(middle_message)
+
+		elif step2 in choice_answer_meaning:
+			print(middle_message)
+
+		elif step2 in choice_answer_collocations:
+			print(middle_message)
+
+		elif step2 in choice_answer_category:
+			print(middle_message)
+			process.del_category(step2)
+
+		else:
+ 			print(final_message)
+
+	elif step in first_answer_exit:
+		break
+
 	else:
 		print(final_message)
-else:
-	print(final_message)
-'''
+		break
