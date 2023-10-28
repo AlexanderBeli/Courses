aa92ba8189c4e270c370b580cb726e37f7fafb38
Y <y@MacBook-Air-Y-2.local>
2023-10-28 12:23:29 +0200

    test_mod7.py updated

--- python/Module_6-7/test_mod7.py
+++ python/Module_6-7/test_mod7.py
@@ -1,1 +1,18 @@
+import requests
 
+r = requests.get("https://api.github.com/users/tater/events")
+print(r)
+print(r.text)
+
+headers = {"X-Request-Id": "<my-request-id>"}
+response = requests.get("https://api.thedogapi.com/v1/breeds", headers=headers)
+print(response.headers)
+request = response.request
+print(request)
+print(request.path_url)
+print(request.method)
+print(request.headers)
+print(response.headers.get("Content-Type"))  #узнали, что json
+response_json = response.json()
+print(response_json) #list
+#print(response_json["name"])
