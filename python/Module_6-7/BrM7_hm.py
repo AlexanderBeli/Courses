8cdc78826465677cd2935ab7ceab114637c8aa2a
AlexandrBeli <alexandrbelitskiy@gmail.com>
2023-11-08 22:32:45 +0100

    Pandas

--- python/Module_6-7/BrM7_hm.py
+++ python/Module_6-7/BrM7_hm.py
@@ -1,7 +1,8 @@
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
-import re
-import matplotlib.pyplot as plt
 
 response = requests.get('http://mfd.ru/currency/?currency=USD')
 
@@ -14,30 +15,41 @@ a = soup.find_all('td')
 #for string in a:
     #print(string.get_text())
 
-currency_data = {}
-currency_date = []
-currency_value = []
-
+#currency_data = {}
+#currency_date = []
+#currency_value = []
+df = pd.DataFrame({})
 d = len(a)
 for x in range(0, d):
     if re.search(r'\d\d.\d\d.\d{4}',str(a[x].string)):
         if re.search(r'\d\d.\d{4}',str(a[x+1].string)):
             name = str(a[x].string).replace('с ','')
-            value = str(a[x+1].string)
-            currency_date.append(name)
-            currency_value.append(value)
+            value = float(a[x+1].string)
+            #currency_date.append(name)
+            #currency_value.append(value)
             #currency_data[f'{name}'] = value
+            temporary_dict = {"Date":name, "USD":value}
+            df1 = pd.DataFrame([temporary_dict])
+            df2 = pd.concat([df1, df], ignore_index=True)
+            df = df2
+
 
 #b = soup.find_all('table')
 #print(type(a))
 #print(a[200].string)
-currency_date.reverse()
-currency_value.reverse()
-print(currency_date)
-print(currency_value)
-for x in range(len(currency_value)):
-    currency_value[x] = float(currency_value[x])
-x = currency_date
-y = currency_value
-plt.plot(x, y)
+
+#currency_date.reverse()
+#currency_value.reverse()
+#print(currency_date)
+#print(currency_value)
+#for x in range(len(currency_value)):
+    #currency_value[x] = float(currency_value[x])
+#x = currency_date
+#y = currency_value
+#plt.plot(x, y)
+
+print(df)
+#plt.close("all")
+#df.plot.line()
+df.plot(x="Date", y="USD", ylabel="Rubles",x_compat=True)
 plt.show()
