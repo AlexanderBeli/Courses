db3937bc0f5cd8e22522bd3074a14f335f8eaea2
AlexandrBeli <alexandrbelitskiy@gmail.com>
2023-11-08 22:33:08 +0100

    Pandas

---
+++ python/Module_6-7/pandas_mod7.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.DataFrame({"Date":[],
+                   "USD":[]})
+
+df.plot()
+plt.show()
+
+'''
+df = pd.DataFrame(np.random.randn(100, 5), columns=list('ABCDE'))
+df=df.cumsum() # Return cumulative sum over a DataFrame or Series axis
+df.plot()
+plt.show()
+'''
