016ad4eb094f5a5d736bf4c53a2b7a0154bb31d9
AlexandrBeli <alexandrbelitskiy@gmail.com>
2023-11-09 17:46:57 +0100

    Pandas Upd

--- python/Module_6-7/BrM7_hm.py
+++ python/Module_6-7/BrM7_hm.py
@@ -16,30 +16,29 @@ a = soup.find_all('td')
     #print(string.get_text())
 
 #currency_data = {}
-#currency_date = []
-#currency_value = []
-df = pd.DataFrame({})
+currency_date = []
+currency_value = []
 d = len(a)
 for x in range(0, d):
     if re.search(r'\d\d.\d\d.\d{4}',str(a[x].string)):
         if re.search(r'\d\d.\d{4}',str(a[x+1].string)):
             name = str(a[x].string).replace('с ','')
             value = float(a[x+1].string)
-            #currency_date.append(name)
-            #currency_value.append(value)
+            currency_date.append(name)
+            currency_value.append(value)
             #currency_data[f'{name}'] = value
-            temporary_dict = {"Date":name, "USD":value}
-            df1 = pd.DataFrame([temporary_dict])
-            df2 = pd.concat([df1, df], ignore_index=True)
-            df = df2
+            #temporary_dict = {"Date":name, "USD":value}
+            #df1 = pd.DataFrame([temporary_dict])
+            #df2 = pd.concat([df1, df], ignore_index=True)
+            #df = df2
 
 
 #b = soup.find_all('table')
 #print(type(a))
 #print(a[200].string)
 
-#currency_date.reverse()
-#currency_value.reverse()
+currency_date.reverse()
+currency_value.reverse()
 #print(currency_date)
 #print(currency_value)
 #for x in range(len(currency_value)):
@@ -47,9 +46,12 @@ for x in range(0, d):
 #x = currency_date
 #y = currency_value
 #plt.plot(x, y)
-
+df = pd.DataFrame({'Date':currency_date, 'USD':currency_value})
 print(df)
 #plt.close("all")
 #df.plot.line()
+df['Date'] = pd.to_datetime(df['Date'],dayfirst=True)
 df.plot(x="Date", y="USD", ylabel="Rubles",x_compat=True)
+plt.title("Курс доллара")
+#plt.ylim(bottom=0)
 plt.show()
