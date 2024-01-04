c35781933fd7ec5002647b453d16382078fdadfa
AlexandrBeli <alexandrbelitskiy@gmail.com>
2024-01-04 08:29:43 +0100

    New updates for Heroku deployment

--- python/PythonCourse/Module_9-10/pages/django_project/settings.py
+++ python/PythonCourse/Module_9-10/pages/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#cb#_j6di%*!y6ubdq669dl51@y&k=l%(3dd@4qmxj!qx++ee(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
